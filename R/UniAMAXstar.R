6d8eb6afc7861e5b0667738893e6ec5ecd489402
Subha Halder <133703969+SubhaHalder-spec@users.noreply.github.com>
2025-08-30 17:43:21 +0530

    Update UniAMAXstar.R

--- R/UniAMAXstar.R
+++ R/UniAMAXstar.R
@@ -102,7 +102,7 @@ UniAMAXstar <- function(sample_data, significance_level){
     return(which.min(res))
   }
   sample_data <- lapply(sample_data, function(x) x[!is.na(x)])
-  num_samples = 20000
+  num_samples = 100000
   num_datasets <- length(sample_data)
   n <- sapply(sample_data, length)
   proportions <- n / sum(n)
@@ -241,3 +241,4 @@ UniAMAXstar <- function(sample_data, significance_level){
   }
   return(paste("Peak:", h, "; UniAMAX* Critical value:", quantile_value, "; UniAMAX* Test statistic:", Aunimax, "; Result:", result))
 }
+
